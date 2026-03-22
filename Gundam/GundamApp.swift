617a9fa5b5d9f4b41f16e592907f69c09373d477
Markyang <markyang940329@gmail.com>
2026-03-22 15:18:02 +0800

    Initial Commit

---
+++ Gundam/GundamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GundamApp.swift
+//  Gundam
+//
+//  Created by 114-2Workshop14 on 2026/3/22.
+//
+
+import SwiftUI
+
+@main
+struct GundamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
